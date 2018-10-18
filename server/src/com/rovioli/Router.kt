0f60ca5c673969f45fa25ada0ba40db997d29f7b
Vitalii Dmitriev <dmitvitalii@gmail.com>
2018-10-18 17:55:56 +0300

    Enhance validation

--- server/src/com/rovioli/Router.kt
+++ server/src/com/rovioli/Router.kt
@@ -6,6 +6,11 @@ import com.rovioli.model.Record
 import com.rovioli.model.Repository
 import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.HttpStatusCode.Companion.Accepted
+import io.ktor.http.HttpStatusCode.Companion.Forbidden
+import io.ktor.http.HttpStatusCode.Companion.Unauthorized
+import io.ktor.http.HttpStatusCode.Companion.UnprocessableEntity
 import io.ktor.request.receiveText
 import io.ktor.response.respond
 import io.ktor.response.respondText
@@ -19,16 +24,27 @@ class Router(private val repository: Repository) {
     fun start(application: Application) {
         application.routing {
             post("/save") {
-                // val result = call.receive<Record>()
-                val jsonText = call.receiveText()
-                val obj = gson.fromJson<Record>(jsonText, Record::class.java)
-                println(jsonText)
-                println("Result object is $obj")
-                call.respond("Got this, ma boooi!")
+                // TODO: can we use call.receive<Record>() instead?
+                val record = gson.fromJson<Record>(call.receiveText(), Record::class.java)
+                println("recorod is $record")
+                val code = validate(record)
+                if (code == Accepted) {
+                    repository.putScore(record.score)
+                    call.respond(code, "Adding!\n")
+                } else {
+                    call.respond(code, "Error!\n")
+                }
             }
             get("/alien") {
                 call.respondText(repository.getAlien())
             }
         }
+    }
+
+    private fun validate(record: Record): HttpStatusCode = with(record) {
+        if (apiKey == null || score.name.isEmpty()) return UnprocessableEntity
+        if (apiKey.isEmpty()) return Unauthorized
+        // if (!keyRegistered(apiKey)) return Forbidden
+        return Accepted
     }
 }
