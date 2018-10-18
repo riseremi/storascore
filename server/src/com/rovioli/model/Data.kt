c3feb8eea03e70a895df7cfe29f36bf503303df2
Vitalii Dmitriev <dmitvitalii@gmail.com>
2018-10-18 17:42:44 +0300

    The apiKey field can be null

--- server/src/com/rovioli/model/Data.kt
+++ server/src/com/rovioli/model/Data.kt
@@ -1,5 +1,5 @@
 package com.rovioli.model
 
-data class Record(val apiKey: String, val score: Score)
+data class Record(val apiKey: String?, val score: Score)
 
 data class Score(val name: String, val score: Int)
