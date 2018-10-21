33a04f4a4582a8a9a5a3f22206d75574dcd4b26d
Vitalii Dmitriev <dmitvitalii@gmail.com>
2018-10-21 17:50:22 +0300

    Add a database helper

---
+++ server/src/com/rovioli/runmaboi/model/RequestAndScoreDatabaseHelper.kt
@@ -0,0 +1,45 @@
+package com.rovioli.runmaboi.model
+
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+
+/**
+ * Pretty ugly helper, but enough for its purpose.
+ */
+class RequestAndScoreDatabaseHelper {
+    fun connect() {
+        Database.connect(
+                url      = "jdbc:h2:mem:test",
+                driver   = "org.h2.Driver",
+                user     = "get_from_config",
+                password = "get_from_config"
+        )
+    }
+
+    fun createTables() = transaction {
+        SchemaUtils.create(Clients)
+        SchemaUtils.create(Scores)
+    }
+
+    fun insertScore(data: Score) = transaction {
+        Scores.insert {
+            it[name]  = data.name
+            it[score] = data.score
+        }
+    }
+
+    fun insertApiKey(key: String) = transaction {
+        Clients.insert { it[apiKey] = key }
+    }
+
+    fun findApiKey(key: String) = transaction {
+        Clients.select { Clients.apiKey eq key }.toString()
+    }
+
+    fun readScores(amount: Int) = transaction {
+        Scores.selectAll()
+                .fetchSize(amount)
+                .orderBy(Scores.score)
+                .toList()
+    }
+}
