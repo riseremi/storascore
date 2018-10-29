04a3f1e43708bb6c20a5703df11f8b5080665288
Vitalii Dmitriev <dmitvitalii@gmail.com>
2018-10-29 20:59:47 +0300

    Introduce AppController interface

---
+++ server/src/com/rovioli/runmaboi/controller/ApiController.kt
@@ -0,0 +1,35 @@
+package com.rovioli.runmaboi.controller
+
+import com.rovioli.runmaboi.model.Repository
+import com.rovioli.runmaboi.util.Delayer
+import com.rovioli.runmaboi.util.generate
+import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+class ApiController(
+        private val repository: Repository,
+        private val delayer: Delayer
+) : AppController {
+
+    override fun attach(application: Application) = application.routing {
+        get("/registerme") {
+            register(call)
+        }
+    }
+
+    private suspend fun register(call: ApplicationCall) {
+        if (delayer.isDelayed()) {
+            call.respond(HttpStatusCode.Forbidden, "Try to register tomorrow")
+        } else {
+            val key = generate()
+            delayer.startDelay()
+            call.respond(HttpStatusCode.OK, key)
+            repository.register(key)
+        }
+    }
+}
