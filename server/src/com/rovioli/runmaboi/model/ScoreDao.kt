9d4f6e12f4a25b7b77b5102add1b8d8334e9fda0
Vitalii Dmitriev <dmitvitalii@gmail.com>
2018-11-01 23:31:52 +0300

    Partially implement ScoreDao

---
+++ server/src/com/rovioli/runmaboi/model/ScoreDao.kt
@@ -0,0 +1,32 @@
+package com.rovioli.runmaboi.model
+
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+
+class ScoreDao(private val helper: RequestAndScoreDatabaseHelper) : AppDao<String, Score> {
+
+    override fun findAll(amount: Int) = transaction {
+        val scores = mutableListOf<Score>()
+        Scores.selectAll()
+                .fetchSize(amount)
+                .orderBy(Scores.score, isAsc = false)
+                .forEach {
+                    scores.add(Score(it[Scores.name], it[Scores.score]))
+                }
+        scores
+    }
+
+    override fun insert(data: Score) = transaction {
+        Scores.insertAndGetId {
+            it[name] = data.name
+            it[score] = data.score
+        }.value
+    }
+
+    override fun find(key: String) = transaction(helper.database) {
+        Scores.select { Scores.name eq key } // TODO: return Score object from DB!
+        Score("Remi", 1488)
+    }
+}
