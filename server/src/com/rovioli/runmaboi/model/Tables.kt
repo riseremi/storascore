01b683ed48027d955d9de9974f9a3f451ef2e48d
Vitalii Dmitriev <dmitvitalii@gmail.com>
2018-11-01 23:50:04 +0300

    Make a name be larger: 8 -> 50 symbols

--- server/src/com/rovioli/runmaboi/model/Tables.kt
+++ server/src/com/rovioli/runmaboi/model/Tables.kt
@@ -3,7 +3,7 @@ package com.rovioli.runmaboi.model
 import com.rovioli.runmaboi.util.API_KEY_LENGTH
 import org.jetbrains.exposed.dao.IntIdTable
 
-const val NAME_LENGTH = 8
+const val NAME_LENGTH = 50
 
 object Clients : IntIdTable() {
     val apiKey = varchar("apiKey", API_KEY_LENGTH)
