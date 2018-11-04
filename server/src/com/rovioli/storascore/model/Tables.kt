503cef6f4ce9efd5b775a7d5d9a2d9a837ef9ed2
Vitalii Dmitriev <dmitvitalii@gmail.com>
2018-11-04 18:01:15 +0300

    Add a Score as a table entity

--- server/src/com/rovioli/storascore/model/Tables.kt
+++ server/src/com/rovioli/storascore/model/Tables.kt
@@ -1,6 +1,9 @@
 package com.rovioli.storascore.model
 
 import com.rovioli.storascore.util.API_KEY_LENGTH
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.IntIdTable
 
 const val NAME_LENGTH = 50
@@ -12,4 +15,11 @@ object Clients : IntIdTable() {
 object Scores : IntIdTable() {
     val name  = varchar("name", NAME_LENGTH)
     val score = integer("score")
+}
+
+class Score(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<Score>(Scores)
+
+    var name  by Scores.name
+    var score by Scores.score
 }
